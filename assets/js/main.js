d22dfffb55c059cbde2c3d32d7b37c38c78453f3
jodrack <drack.sir01@gmail.com>
2022-04-11 23:42:44 +0200

    Implement function to generate programs data dynamically

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,30 @@
 import programs from './data/programs';
 import speakers from './data/speakers';
+
+// generate cards automatically
+const generateProgramCards = () => {
+  const cardsWrapper = document.querySelector('.cards-wrapper');
+  cardsWrapper.innerHTML = '';
+
+  for (let i = 0; i < programs.length; i += 1) {
+    cardsWrapper.innerHTML += `
+    <div class="card">
+        <div>
+            <img
+            src="${programs[i].image}"
+            id="${
+              programs[i].title.toLocaleLowerCase() === 'boost' ? 'boost' : ''
+            }"
+            alt="${programs[i].title} icon"
+            />
+        </div>
+        <h3>${programs[i].title}</h3>
+        <p>
+            ${programs[i].description}
+        </p>
+    </div>
+    `;
+  }
+};
+
+generateProgramCards();
