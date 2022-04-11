64b0b5b711d57acaddda2cfc2ff93639724c55e0
jodrack <drack.sir01@gmail.com>
2022-04-11 23:36:38 +0200

    Add programs details inside a collection of js objects to generate content dynamically

---
+++ assets/js/data/programs.js
@@ -0,0 +1,29 @@
+const programs = [
+  {
+    title: 'Lecture',
+    image: './assets/images/icons/lecture.svg',
+    description: 'Listening to speakers from different Rwanda areas.',
+  },
+  {
+    title: 'Exhibition',
+    image: './assets/images/icons/exhibition.svg',
+    description: 'Listening to speakers from different Rwanda areas',
+  },
+  {
+    title: 'Workshop',
+    image: './assets/images/icons/workshop.svg',
+    description: 'Listening to speakers from different Rwanda areas',
+  },
+  {
+    title: 'Boost',
+    image: 'https://cdn-icons-png.flaticon.com/512/1067/1067357.png',
+    description: 'Listening to speakers from different Rwanda areas.',
+  },
+  {
+    title: 'Talk',
+    image: './assets/images/icons/forum.svg',
+    description: 'Listening to speakers from different Rwanda areas.',
+  },
+];
+
+export default programs;
