c89e51b2208d826763351ec7f9281e3720f228ac
jodrack <drack.sir01@gmail.com>
2022-04-11 23:46:56 +0200

    Implement function generateSpeakersData to generate data dynamically

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,4 +27,31 @@ const generateProgramCards = () => {
   }
 };
 
+// generating speakers data dynamically
+const generateSpeakersData = () => {
+  const speakersWrapper = document.querySelector('.card-wrapper');
+  speakersWrapper.innerHTML = '';
+  for (let i = 0; i < speakers.length; i += 1) {
+    speakersWrapper.innerHTML += `
+    <div class="speaker-card">
+      <div class="speaker-card-images">
+        <img src="./assets/images/img1.png" alt="background">
+        <img
+          src="${speakers[i].image}"
+          alt="speaker-${i} image"
+        />
+      </div>
+      <div class="speaker-details">
+        <h3>${speakers[i].name}</h3>
+        <p class="speaker-desc-1">${speakers[i].description1}</p>
+        <hr>
+        <p class="speaker-desc-2">${speakers[i].description2}</p>
+      </div>
+    </div>
+  `;
+  }
+};
+
+// invoking functions
 generateProgramCards();
+generateSpeakersData();
